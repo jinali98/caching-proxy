a98d6c52acd5d04139958b84ecaa75b0fc6b4013
jinali98 <jinalipabasara@gmail.com>
2026-09-19 13:14:37 +0530

    init project

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func main() {
+	port := flag.Int("port", 0, "port to run the caching proxy on")
+	origin := flag.String("origin", "", "origin server URL to forward requests to")
+	clearCache := flag.Bool("clear-cache", false, "clear the cache and exit")
+
+	flag.Parse()
+
+	if *clearCache {
+		fmt.Println("cache cleared")
+		return
+	}
+
+	if err := run(*port, *origin); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
+
+func run(port int, origin string) error {
+
+	if err := validateOrigin(origin); err != nil {
+		return err
+	}
+
+	if err := validatePort(port); err != nil {
+		return err
+	}
+
+	p := &proxy{
+		origin: origin,
+	}
+
+	addr := fmt.Sprintf(":%d", port)
+
+	fmt.Printf("proxying %s -> %s\n", addr, origin)
+
+	return http.ListenAndServe(addr, p)
+}
+
+type proxy struct {
+	origin string
+}
+
+func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "would forward %s to %s%s\n", r.Method, p.origin, r.URL.RequestURI())
+}
+
+func validatePort(p int) error {
+	if p < 1 || p > 65535 {
+		return fmt.Errorf("invalid port %d: must be between 1 and 65535", p)
+	}
+
+	return nil
+}
+
+func validateOrigin(o string) error {
+	if strings.TrimSpace(o) == "" {
+		return errors.New("origin cannot be empty")
+	}
+
+	u, err := url.Parse(o)
+
+	if err != nil {
+		return fmt.Errorf("invalid origin %q: %w", o, err)
+	}
+
+	if strings.TrimSpace(u.Host) == "" {
+		return errors.New("invalid host")
+
+	}
+
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return fmt.Errorf("origin %q must use http or https, got %q", o, u.Scheme)
+	}
+
+	return nil
+}
